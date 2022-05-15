a069e28eef82754599f6e85fbf73d31429af3107
dima-starkov <62872531+dima-starkov@users.noreply.github.com>
2022-05-15 16:20:46 +0300

    Initial Commit

---
+++ BloggingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BloggingApp
+//
+//  Created by Дмитрий Старков on 15.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
